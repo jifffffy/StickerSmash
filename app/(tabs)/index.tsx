960a9ea3c4c71f21118a139a1c174342c262d475
Jiffy <jifffffy@gmail.com>
2025-01-18 15:10:25 +0800

    capture a screenshot and save it

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -58,6 +58,19 @@ export default function Index() {
   }
 
   const onSaveImagesAsync = async () => {
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert('Saved!');
+      }
+    } catch (e) {
+      console.log(e);
+    }
 
   }
 
